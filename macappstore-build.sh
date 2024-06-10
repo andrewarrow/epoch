db42af8603f37d735d3b4c4997a4e30d9e6072a6
Andrew Arrow <oneone@gmail.com>
2024-06-10 07:20:52 -0700

    added support for new data source

--- macappstore-build.sh
+++ macappstore-build.sh
@@ -5,6 +5,6 @@ PKG_CERTIFICATE="3rd Party Mac Developer Installer: Andrew Arrow (755R3VBZ84)"
 
 cp ~/Documents/store.provisionprofile epoch.app/Contents
 
-codesign --timestamp --options=runtime -s "$APP_CERTIFICATE" -v --entitlements ./build/darwin/entitlements.plist ./build/bin/epoch.app
+codesign --timestamp --options=runtime -s "$APP_CERTIFICATE" -v --entitlements ./build/darwin/entitlements.plist epoch.app
 
-productbuild --sign "$PKG_CERTIFICATE" --component ./build/bin/epoch.app /Applications ./epoch.pkg
+productbuild --sign "$PKG_CERTIFICATE" --component epoch.app /Applications ./epoch.pkg
