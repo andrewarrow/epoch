88752320dc51acfd62ccb08e21c2fa1574ad5104
Andrew Arrow <oneone@gmail.com>
2024-06-09 19:50:52 -0700

    added support for new data format

--- sound/setup.go
+++ sound/setup.go
@@ -8,6 +8,9 @@ import (
 )
 
 func PlaySound(filename string) {
+	if true {
+		return
+	}
 	asBytes, _ := os.ReadFile("assets/sounds/done5.mp3")
 	decoder, data, _ := minimp3.DecodeFull(asBytes)
 	context, _ := oto.NewContext(decoder.SampleRate, 2, 2, 4096)
